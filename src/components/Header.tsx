28bb77bef4b326ecdbbaadf8a7dd44c5adc4d59a
NaniasNic <ananiasdias0@gmail.com>
2023-06-29 02:40:30 -0300

    Header and Semantic: Created

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+export function Header() {
+    return (
+        <header className="w-full h-32 flex items-center justify-around">
+            <div>
+                <h1 className="text-5xl">
+                    Simply
+                    <span className="text-gray-400">.</span>
+                </h1>
+            </div>
+
+            <nav>
+                <ul className="flex gap-6">
+                    <li className="uppercase font-semibold">Home</li>
+                    <li className="uppercase font-semibold">Portfolio</li>
+                    <li className="uppercase font-semibold">contact</li>
+                    <li className="uppercase font-semibold">Blog</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
