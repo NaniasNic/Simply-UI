1fbe50b8ec5156b515994294ef34acc7915dcbee
NaniasNic <ananiasdias0@gmail.com>
2023-07-08 16:18:04 -0300

    PrimaryContent and SecondaryContent: responsive designer create

--- src/components/PrimaryContent.tsx
+++ src/components/PrimaryContent.tsx
@@ -2,10 +2,10 @@ import { PlayCircle } from "lucide-react";
 
 export function PrimaryContent() {
     return (
-        <main className="flex flex-col gap-8 justify-center items-center px-48 py-[60px]">
-            <h1 className="text-5xl font-bold uppercase">Featured Content</h1>
+        <main className="flex flex-col gap-8 justify-center items-center px-48 py-[60px] max-[900px]:px-20 max-[400px]:px-10">
+            <h1 className="text-5xl font-bold uppercase text-center max-[400px]:text-3xl">Featured Content</h1>
             
-            <hr className="w-80 border-b-2 border-black" />
+            <hr className="w-80 border-b-2 border-black max-[400px]:w-60" />
             
             <p className="text-center">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id, dolore quis. 
@@ -14,7 +14,7 @@ export function PrimaryContent() {
             </p>
 
             <div className="flex justify-center items-center">
-                <PlayCircle className="absolute text-gray-400 w-40 h-40 cursor-pointer hover:text-gray-50" />
+                <PlayCircle className="absolute text-gray-400 w-40 h-40 max-[460px]:h-20 cursor-pointer hover:text-gray-50" />
                 
                 <img src="/lake.jpg" alt="lake" className="w-full" />
             </div>
--- src/components/SecondaryContent.tsx
+++ src/components/SecondaryContent.tsx
@@ -1,39 +1,39 @@
 export function SecondaryContent() {
     return (
-        <main className="flex py-[60px] px-0 gap-10 justify-center items-center">
-            <div className="flex bg-[url('/article-image-1.jpg')] bg-cover items-center border w-[500px] h-[300px] gap-5">
-                <div className="flex w-[230px] h-full bg-white/95 gap-4 flex-col justify-center items-center p-5">
-                    <h1 className="text-4xl text-center font-bold">Secondary Content</h1>
+        <main className="flex py-[60px] px-0 gap-10 justify-center items-center max-[1080px]:flex-col">
+            <div className="flex bg-[url('/article-image-1.jpg')] bg-cover items-center border w-[500px] h-[300px] gap-5 max-[540px]:w-[400px] max-[430px]:w-[300px] max-[430px]:h-[250px]">
+                <div className="flex w-[230px] h-full bg-white/95 gap-4 flex-col justify-center items-center p-5 max-[540px]:w-[200px]">
+                    <h1 className="text-4xl text-center font-bold max-[540px]:text-2xl max-[430px]:text-xl">Secondary Content</h1>
                     
                     <hr className="w-36 border border-[#02b8dd]" />
                     
-                    <p className="text-center text-[#02b8dd]">
+                    <p className="text-center text-[#02b8dd] max-[430px]:text-sm">
                         <small>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                             Donec euismod leo a nibh dignissim tincidunt nam.
                         </small> 
                     </p>
                     
-                    <a href="#" className="border border-[#02b8dd] text-[#02b8dd] rounded-sm px-5 py-[6px]">
+                    <a href="#" className="border border-[#02b8dd] text-[#02b8dd] rounded-sm px-5 py-[6px] max-[430px]:px-3 max-[430px]:py-2">
                         View More
                     </a>
                 </div>
             </div>
             
-            <div className="flex bg-[url('/article-image-2.jpg')] bg-cover items-center border w-[500px] h-[300px] gap-5">
-                <div className="flex w-[230px] h-full bg-white/95 gap-4 flex-col justify-center items-center p-5">
-                    <h1 className="text-4xl text-center font-bold">Secondary Content</h1>
+            <div className="flex bg-[url('/article-image-2.jpg')] bg-cover items-center border w-[500px] h-[300px] gap-5 max-[540px]:w-[400px] max-[430px]:w-[300px] max-[430px]:h-[250px]">
+                <div className="flex w-[230px] h-full bg-white/95 gap-4 flex-col justify-center items-center p-5 max-[540px]:w-[200px]">
+                    <h1 className="text-4xl text-center font-bold max-[540px]:text-2xl max-[430px]:text-xl">Secondary Content</h1>
                     
                     <hr className="w-36 border border-[#02b8dd]" />
                     
-                    <p className="text-center text-[#02b8dd]">
+                    <p className="text-center text-[#02b8dd] max-[430px]:text-sm">
                         <small>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                             Donec euismod leo a nibh dignissim tincidunt nam.
                         </small> 
                     </p>
                     
-                    <a href="#" className="border border-[#02b8dd] text-[#02b8dd] rounded-sm px-5 py-[6px]">
+                    <a href="#" className="border border-[#02b8dd] text-[#02b8dd] rounded-sm px-5 py-[6px] max-[430px]:px-3 max-[430px]:py-2">
                         View More
                     </a>
                 </div>
